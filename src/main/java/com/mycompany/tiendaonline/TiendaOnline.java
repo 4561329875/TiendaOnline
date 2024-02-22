a1b480aabe3ea839ddc704398cf8c71b9fdfb2a3
David <fdtoro@espe.edu.ec>
2024-02-22 07:22:05 -0500

    Vr.0.0

---
+++ src/main/java/com/mycompany/tiendaonline/TiendaOnline.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tiendaonline;
+
+/**
+ *
+ * @author labctr
+ */
+public class TiendaOnline {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
